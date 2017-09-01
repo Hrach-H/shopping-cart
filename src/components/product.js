6731eb904e67d908e122d0b589113a028b005eda
hrachya.hovsepyan <hrachya.hovsepyan@betconstruct.com>
2017-09-01 11:58:23 +0400

    Improved 'Cart' functionality

--- src/components/product.js
+++ src/components/product.js
@@ -11,6 +11,7 @@ class Product extends Component {
         store.dispatch(addToCart({
             id: this.props.id,
             name: this.props.name,
+            price: this.props.price,
             quantity: this.props.cart[this.props.id] ?
                 (this.props.cart[this.props.id].quantity < this.props.availableQuantity ? this.props.cart[this.props.id].quantity+1 : this.props.cart[this.props.id].quantity) : 1
         }));
--- src/components/second-page.js
+++ src/components/second-page.js
@@ -3,12 +3,18 @@ import { connect } from 'react-redux';
 
 class secondPage extends Component {
 
+    renderCartContents() {
+       return Object.keys(this.props.cart).map( (order) => {
+           return <li key={order}>You have ordered {this.props.cart[order].quantity} unit(s) of {this.props.cart[order].name}</li>
+       } )
+    }
+
     render() {
         return (
             <div>
                 <h2>Your shopping cart contents</h2>
                 <ul>
-
+                    {(Object.keys(this.props.cart).length > 1 && this.renderCartContents()) || "You haven't ordered yet"}
                 </ul>
 
             </div>
@@ -16,6 +22,6 @@ class secondPage extends Component {
     }
 }
 
-secondPage = connect( (state) => ({ cart: state.cart}) )(secondPage);
+secondPage = connect( (state) => ({ cart: state.cartReducer}) )(secondPage);
 
 export default secondPage;
